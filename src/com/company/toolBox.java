1b4ca1ec971100ee048fc074f7eaa1efb8f4dbb4
Owen Riske <owenr0129@5565.ca>
2023-05-26 09:09:39 -0600

    Fixed the randomPosition

--- src/com/company/toolBox.java
+++ src/com/company/toolBox.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class toolBox {
     public static int randomPosition(int min, int max){
         Random rand=new Random();
-        return rand.nextInt(max+min)-min;
+        return rand.nextInt(max-min)+min;
     }
 
 
