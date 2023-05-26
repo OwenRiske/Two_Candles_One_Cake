c0bfc24232c8e7225358ebc4e125b7a219291fa4
Owen Riske <owenr0129@5565.ca>
2023-05-26 08:55:21 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int max=100;
+        int min=0;
+        double success=0;
+        double failure=0;
+        for (int i = 0; i < 999999; i++) {
+            int candle1=toolBox.randomPosition(min,max);
+            int candle2=toolBox.randomPosition(min,max);
+            int cut=toolBox.randomPosition(min,max);
+            if(candle1<cut && cut<candle2 || candle1>cut && cut>candle2){
+                success++;
+            }
+            else{
+                failure++;
+            }
+
+        }
+        System.out.println(success/failure*100+"%");
+    }
+}
---
+++ src/com/company/toolBox.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Random;
+
+
+public class toolBox {
+    public static int randomPosition(int min, int max){
+        Random rand=new Random();
+        return rand.nextInt(max+min)-min;
+    }
+
+
+}
